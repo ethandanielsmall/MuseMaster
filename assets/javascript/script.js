42212483ab532e408fb76be8e4e347bfe07097df
christopherflores9312 <christopherflores9312@gmail.com>
2023-03-30 21:14:04 -0700

    added advice api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,3 +26,24 @@ const fetchInspirationButton = document.getElementById('fetchInspiration');
 
 // Add a click event listener to the 'fetchButton' that calls the 'fetchData' function when the button is clicked
 fetchInspirationButton.addEventListener('click', fetchInspiration);
+
+// Define a function to fetch data from the Advice Slip API using fetch (Advice)
+function fetchAdvice() {
+    // Construct the URL for the Advice Slip API request
+    const url = "https://api.adviceslip.com/advice";
+
+    // Make a fetch request to the Advice Slip API
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Find the HTML element with the ID 'quote' and update its text content
+            // to display the advice received from the API response (data)
+            document.getElementById('quote').textContent = data.slip.advice;
+        })
+        .catch(error => {
+            console.error('Error fetching advice:', error);
+        });
+}
+
+const fetchAdviceButton = document.getElementById('fetchAdvice');
+fetchAdviceButton.addEventListener('click', fetchAdvice);
