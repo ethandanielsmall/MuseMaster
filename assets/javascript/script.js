a5f67a2a350dc6eb9169205201e88cb42fa917bb
christopherflores9312 <christopherflores9312@gmail.com>
2023-04-03 19:03:17 -0700

    edited comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -24,20 +24,19 @@ function callbackFunction(data) {
 // Find the button element with the ID 'fetchButton' and store it in the variable 'fetchButton'
 const fetchInspirationButton = document.getElementById('fetchInspiration');
 
-// Add a click event listener to the 'fetchButton' that calls the 'fetchData' function when the button is clicked
+//event listener to the 'fetchButton' that calls the 'fetchData' function when the button is clicked
 fetchInspirationButton.addEventListener('click', fetchInspiration);
 
-// Define a function to fetch data from the Advice Slip API using fetch (Advice)
+//fetch data from the Advice Slip API using fetch (Advice)
 function fetchAdvice() {
-    // Construct the URL for the Advice Slip API request
+    //URL for the Advice Slip API request
     const url = "https://api.adviceslip.com/advice";
 
-    // Make a fetch request to the Advice Slip API
+    //Advice Slip API
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            // Find the HTML element with the ID 'quote' and update its text content
-            // to display the advice received from the API response (data)
+            //display the advice received from the API response (data)
             document.getElementById('quote').textContent = data.slip.advice;
         })
         .catch(error => {
